a42b1ece600b07b20370eb2488fb4b237b1e735f
Nikolai Petukhov <petukhov.niko@gmail.com>
2024-12-04 12:50:41 -0300

    init

---
+++ src/main.py
@@ -0,0 +1,69 @@
+from js import ImageData, Object, slyApp
+from pyodide.ffi import create_proxy
+import numpy as np
+import cv2
+
+
+def dump(obj):
+  for attr in dir(obj):
+    print("obj.%s = %r" % (attr, getattr(obj, attr)))
+
+
+def main(mode='process'):
+  app = slyApp.app
+  store = slyApp.store
+  app = getattr(app, '$children')[0]
+
+  context = app.context
+  state = app.state
+
+  # store action example
+  # appEventEmitter = app.appEventEmitter
+  # eventData = Object()
+  # eventData.action = 'videos/nextImage'
+  # eventData.payload = {}
+  # appEventEmitter.emit('store-action', eventData)
+
+  cur_img = getattr(store.state.videos.all, str(context.imageId))
+  img_src = cur_img.sources[0]
+  img_cvs = img_src.imageData
+
+  img_ctx = img_cvs.getContext("2d")
+
+  if state.imagePixelsDataImageId != context.imageId:
+    img_data = img_ctx.getImageData(0, 0, img_cvs.width, img_cvs.height).data
+
+    # reshape flat array of rgba to numpy
+    state.imagePixelsData = np.array(img_data, dtype=np.uint8).reshape(img_cvs.height, img_cvs.width, 4)
+    state.imagePixelsDataImageId = context.imageId
+
+  new_img_data = None
+  img_arr = state.imagePixelsData
+
+  if mode == 'restore':
+    new_img_data = img_arr.flatten()
+  else:
+    img_gray = cv2.cvtColor(img_arr, cv2.COLOR_RGBA2GRAY)
+
+    clip_limit = state.SliderAutoId6MqE3.value
+    clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=(8, 8))
+
+    cl_img_gray = clahe.apply(img_gray)
+    cl_img_rgb = cv2.cvtColor(cl_img_gray, cv2.COLOR_GRAY2RGB)
+
+    alpha_channel = img_arr[:, :, 3]
+    cl_img_rgba = np.dstack((cl_img_rgb, alpha_channel))
+
+    new_img_data = cl_img_rgba.flatten().astype(np.uint8)
+
+  pixels_proxy = create_proxy(new_img_data)
+  pixels_buf = pixels_proxy.getBuffer("u8clamped")
+  new_img_data = ImageData.new(pixels_buf.data, img_cvs.width, img_cvs.height)
+
+  img_ctx.putImageData(new_img_data, 0, 0)
+  img_src.version += 1
+
+  pixels_proxy.destroy()
+  pixels_buf.release()
+
+main
