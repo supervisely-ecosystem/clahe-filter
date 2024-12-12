c989adc48385bbe35413588b6902813d2a4b104f
vorozhkog <vorozhkog@icloud.com>
2024-12-12 14:12:32 +0000

    checkbox for lightness

--- src/main.py
+++ src/main.py
@@ -8,6 +8,14 @@ def dump(obj):
   for attr in dir(obj):
     print("obj.%s = %r" % (attr, getattr(obj, attr)))
 
+def clahe(img_rgb_8_bits, clipLimit=2.0, titleGridSize=(8, 8)):
+    img_lab = cv2.cvtColor(img_rgb_8_bits, cv2.COLOR_BGR2LAB)
+    lab_planes = cv2.split(img_lab)
+    clahe = cv2.createCLAHE(clipLimit=clipLimit, tileGridSize=titleGridSize)
+    lab_planes[0] = clahe.apply(lab_planes[0])
+    img_lab = cv2.merge(lab_planes)
+    img_rgb_8_bits = cv2.cvtColor(img_lab, cv2.COLOR_LAB2BGR)
+    return img_rgb_8_bits
 
 def main(mode='process'):
   app = slyApp.app
@@ -37,24 +45,27 @@ def main(mode='process'):
     state.imagePixelsData = np.array(img_data, dtype=np.uint8).reshape(img_cvs.height, img_cvs.width, 4)
     state.imagePixelsDataImageId = context.imageId
 
+
   new_img_data = None
   img_arr = state.imagePixelsData
 
   if mode == 'restore':
     new_img_data = img_arr.flatten()
   else:
-    img_gray = cv2.cvtColor(img_arr, cv2.COLOR_RGBA2GRAY)
-
     clip_limit = state.SliderAutoId6MqE3.value
-    clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=(8, 8))
+    if state.labCheck is False:
+      img_gray = cv2.cvtColor(img_arr, cv2.COLOR_RGBA2GRAY)
+      clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=(8, 8))
 
-    cl_img_gray = clahe.apply(img_gray)
-    cl_img_rgb = cv2.cvtColor(cl_img_gray, cv2.COLOR_GRAY2RGB)
+      cl_img_gray = clahe.apply(img_gray)
+      cl_img_rgb = cv2.cvtColor(cl_img_gray, cv2.COLOR_GRAY2RGB)
 
-    alpha_channel = img_arr[:, :, 3]
-    cl_img_rgba = np.dstack((cl_img_rgb, alpha_channel))
+      alpha_channel = img_arr[:, :, 3]
+      cl_img_rgba = np.dstack((cl_img_rgb, alpha_channel))
 
-    new_img_data = cl_img_rgba.flatten().astype(np.uint8)
+      new_img_data = cl_img_rgba.flatten().astype(np.uint8)
+    else:
+      new_img_data = clahe(img_arr, clip_limit)
 
   pixels_proxy = create_proxy(new_img_data)
   pixels_buf = pixels_proxy.getBuffer("u8clamped")
